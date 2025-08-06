c2c850257904fc2b69a1aebb7fdb7b63e3010f2b
Jude Woolls <jwoolls01@gmail.com>
2025-08-06 18:19:06 +0100

    add some styling chnages and install whitenoise

--- core/settings.py
+++ core/settings.py
@@ -151,3 +151,13 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'secondary',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',  # 👈 maps error → red alert
+}
