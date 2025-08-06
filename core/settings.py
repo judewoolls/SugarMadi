7bf33b1b804a09246ad8e1b4d69b85570adcca8c
Jude Woolls <jwoolls01@gmail.com>
2025-08-06 18:26:29 +0100

    fix the allwoed host issue

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*herokuapp.com','localhost',]
+ALLOWED_HOSTS = ['*.herokuapp.com','localhost',]
 
 
 # Application definition
